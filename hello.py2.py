e76ae990335a975a592c3d2b67034a40968b683d
Ibrohimjon <77103255+Inrohimjon@users.noreply.github.com>
2023-02-13 16:10:31 +0900

    #12 Dars qo'shildi
    yangiliklar qo'shildi

---
+++ hello.py2.py
@@ -0,0 +1,1 @@
+users = ['alisher1983','aziza','yasina', 'umar']login = input("Yangi login tanlang:")if login in users:    print('Login band, yangi login tanlang!')else:    print(f"Xush kelibsiz! {login.title()}")
