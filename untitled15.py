13fb988cb66c5c3ee7da020b7e6b1f2d680b79b8
Ibrohimjon <77103255+Inrohimjon@users.noreply.github.com>
2023-07-05 15:19:58 +0900

    #Amaliy mashqar
    so'z topish oyini

--- untitled15.py
+++ untitled15.py
@@ -1,7 +1,2 @@
-import multiplier_sonlar_mod
-def son_topish_oyna(x=10):  
-  offer=int(input("Assalomu alaykum!\n Bu o\'yin son topish o'yini. Biron bir son tanlang:"))
-  print(multiplier_sonlar_mod.son_top(offer))
-  print(multiplier_sonlar_mod.son_top_pc(offer))
-  print(multiplier_sonlar_mod.play(x=10))
-  
+from multiplier_sonlar_mod import play
+play()
